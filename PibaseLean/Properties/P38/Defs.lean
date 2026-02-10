26d9f49ced066bae460c44a6a51aae1e1fe946f8
Batixx <s59fpern@uni-bonn.de>
2026-02-10 09:58:54 +0100

    add namespace

--- PibaseLean/Properties/P38/Defs.lean
+++ PibaseLean/Properties/P38/Defs.lean
@@ -2,8 +2,12 @@ import Mathlib
 
 open Topology Set Function
 
+namespace PiBase
+
 /- 38. Injectively path connected -/
 class InjPathConnectedSpace (X : Type u) [TopologicalSpace X] : Prop where
   nonempty : Nonempty X
   joined : Pairwise fun x y : X ↦
     ∃ f : Icc (0 : ℝ) 1 → X, Continuous f ∧ Injective f ∧ f 0 = x ∧ f 1 = y
+
+end PiBase
