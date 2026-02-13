79b72a7f1fbf3fcb0e252cf485ea0ac1a684426f
Batixx <s59fpern@uni-bonn.de>
2026-02-13 16:40:26 +0100

    add format formal theorem

--- PibaseLean/Theorems/T1/Formal.lean
+++ PibaseLean/Theorems/T1/Formal.lean
@@ -2,10 +2,11 @@ import PibaseLean.Theorems.T1.Theorem
 
 namespace PiBase.Formal
 
-abbrev T1 := CompactSpace.CountablyCompactSpace
+abbrev T1 {X : Type*} [TopologicalSpace X] [CompactSpace X] :=
+  CompactSpace.CountablyCompactSpace (X := X)
 
 /-- Contrapose -/
-instance T39_contra {X : Type*} [TopologicalSpace X] [h : NP16 X] : NP19 X where
-  not_p19 := fun a ↦ h.not_p16 (CompactSpace.CountablyCompactSpace)
+instance T39_contra {X : Type*} [TopologicalSpace X] [h : NP19 X] : NP16 X where
+  not_p16 _ := h.not_p19 (CompactSpace.CountablyCompactSpace (X := X))
 
 end PiBase.Formal
--- PibaseLean/Theorems/T39/Formal.lean
+++ PibaseLean/Theorems/T39/Formal.lean
@@ -2,10 +2,11 @@ import PibaseLean.Theorems.T39.Theorem
 
 namespace PiBase.Formal
 
-abbrev T39 := InjPathConnectedSpace.PrePathConnectedSpace
+abbrev T39 {X : Type*} [TopologicalSpace X] [InjPathConnectedSpace X] :=
+  InjPathConnectedSpace.PrePathConnectedSpace (X := X)
 
 /-- Contrapose -/
 instance T39_contra {X : Type*} [TopologicalSpace X] [h : NP37 X] : NP38 X where
-  not_p38 _ := h.not_p37 (InjPathConnectedSpace.PrePathConnectedSpace X)
+  not_p38 _ := h.not_p37 (InjPathConnectedSpace.PrePathConnectedSpace (X := X))
 
 end PiBase.Formal
