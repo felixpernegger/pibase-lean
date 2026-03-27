28cffc8ad649517362354c0027581349a06a3708
Batixx <s59fpern@uni-bonn.de>
2026-03-27 14:05:43 +0100

    wrong uid for t1

--- PibaseLean/Theorems/T1/Formal.lean
+++ PibaseLean/Theorems/T1/Formal.lean
@@ -6,7 +6,7 @@ abbrev T1 {X : Type*} [TopologicalSpace X] [CompactSpace X] :=
   CompactSpace.CountablyCompactSpace (X := X)
 
 /-- Contrapose -/
-instance T39_contra {X : Type*} [TopologicalSpace X] [h : NP19 X] : NP16 X where
+instance T1_contra {X : Type*} [TopologicalSpace X] [h : NP19 X] : NP16 X where
   not_p16 _ := h.not_p19 (CompactSpace.CountablyCompactSpace (X := X))
 
 end PiBase.Formal
