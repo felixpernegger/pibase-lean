794be8ffd03557f7d4c564623dbbe3ab229b4592
Batixx <s59fpern@uni-bonn.de>
2026-03-27 13:46:29 +0100

    remove some formal files

--- PibaseLean/UnstableProperties/P32/Formal.lean
+++ PibaseLean/UnstableProperties/P32/Formal.lean
@@ -2,7 +2,9 @@ import PibaseLean.UnstableProperties.P32.Defs
 
 namespace PiBase.Formal
 
-abbrev P32 := CountablyParacompactSpace
+universe u
+
+abbrev P32 (X : Type u) [TopologicalSpace X] := CountablyParacompactSpace X
 
 class NP32 (X : Type*) [TopologicalSpace X] where
   not_p32 : ¬ P32 X
