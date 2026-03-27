40cbf34b067d55fe441e45f0007147a9fa87879e
Batixx <s59fpern@uni-bonn.de>
2026-03-27 14:04:25 +0100

    worng uid for p1 (lol)

--- PibaseLean/Properties/P1/Formal.lean
+++ PibaseLean/Properties/P1/Formal.lean
@@ -2,9 +2,9 @@ import PibaseLean.Properties.P1.Defs
 
 namespace PiBase.Formal
 
-abbrev P37 := T0Space
+abbrev P1 := T0Space
 
 class NP1 (X : Type*) [TopologicalSpace X] where
-  not_p1 : ¬ P37 X
+  not_p1 : ¬ P1 X
 
 end PiBase.Formal
