8d0410d27d9eecb02d7e707aa2d9e5c6237bd51b
Batixx <s59fpern@uni-bonn.de>
2026-03-27 13:51:36 +0100

    fix p32

--- PibaseLean/UnstableProperties/P32/Defs.lean
+++ PibaseLean/UnstableProperties/P32/Defs.lean
@@ -9,8 +9,8 @@ namespace PiBase
 /- 32. Countably paracompact -/
 class CountablyParacompactSpace (X : Type u) [TopologicalSpace X] : Prop where
   countably_paracompact :
-    ∀ (α : Type v) (s : α → Set X), (∀ a, IsOpen (s a)) → (⋃ a, s a = univ) →
-      ∃ (β : Type v) (t : β → Set X), Countable β →
+    ∀ (α : Type u) (s : α → Set X), (∀ a, IsOpen (s a)) → (⋃ a, s a = univ) →
+      ∃ (β : Type) (t : β → Set X), Countable β →
         (∀ b, IsOpen (t b)) ∧ (⋃ b, t b = univ) ∧ LocallyFinite t ∧ ∀ b, ∃ a, t b ⊆ s a
 
 end PiBase
