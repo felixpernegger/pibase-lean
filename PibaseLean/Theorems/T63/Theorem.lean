f1fa13be090256d63ca65b6f545321b92585f4a8
Batixx <s59fpern@uni-bonn.de>
2026-04-03 19:52:58 +0200

    update p43

--- PibaseLean/Theorems/T63/Theorem.lean
+++ PibaseLean/Theorems/T63/Theorem.lean
@@ -5,13 +5,19 @@ open Topology Set Function
 
 namespace PiBase
 
+open PiBase.AdditionalDefs
+
+#check IsInjPathConnected.IsPathConnected
+
 /- Theorem 63: a locally injectively path connected space
 is locally path connected -/
 instance instLocally {X : Type*} [TopologicalSpace X] [h : LocallyInjPathConnectedSpace X] :
     LocPathConnectedSpace X where
   path_connected_basis x := by
     apply Filter.hasBasis_self.mpr (fun t ht ↦ ?_)
-    #check (Filter.hasBasis_self).1 <| h.inj_path_connected_basis x
+    obtain ⟨r, xr, hr, rt⟩ := (Filter.hasBasis_self).1 (h.inj_path_connected_basis x) t ht
+    have : r.Nonempty := by sorry
+    use r, xr, (IsInjPathConnected.IsPathConnected)
     sorry
 
 
