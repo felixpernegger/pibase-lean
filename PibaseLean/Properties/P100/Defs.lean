fd1c4b4e321c505ff44779210e69cee5ccc22c36
Batixx <s59fpern@uni-bonn.de>
2026-04-04 18:19:34 +0200

    rename kc

--- PibaseLean/Properties/P100/Defs.lean
+++ PibaseLean/Properties/P100/Defs.lean
@@ -5,14 +5,14 @@ open Topology Set Function Filter TopologicalSpace
 namespace PiBase
 
 /- 100. KC Space -/
-class KcSpace (X : Type*) [TopologicalSpace X] : Prop where
+class KC (X : Type*) [TopologicalSpace X] : Prop where
   kc : ∀ s : Set X, IsCompact s → IsClosed s
 
 end PiBase
 
 namespace PiBase.Formal
 
-abbrev P100 := KcSpace
+abbrev P100 := KC
 
 class NP100 (X : Type*) [TopologicalSpace X] where
   not_p100 : ¬ P100 X
