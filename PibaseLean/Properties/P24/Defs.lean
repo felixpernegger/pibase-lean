183c2104a61c6f3e0a02a5917f96e8d9c35bdb82
Batixx <s59fpern@uni-bonn.de>
2026-04-04 18:21:22 +0200

    rename p24

--- PibaseLean/Properties/P24/Defs.lean
+++ PibaseLean/Properties/P24/Defs.lean
@@ -6,7 +6,7 @@ open Function Set Filter Topology TopologicalSpace
 namespace PiBase
 
 /- 24. Locally relatively compact -/
-class LocallyRelativelyCompact (X : Type*) [TopologicalSpace X] : Prop where
+class LocallyRelativelyCompactSpace (X : Type*) [TopologicalSpace X] : Prop where
   locally_relatively_compact : ∀ x : X, (𝓝 x).HasBasis (fun s => IsCompact (closure s)) id
 
 end PiBase
