b6c61839c1ff1820dea32c6e2b53505f69d3b413
Batixx <s59fpern@uni-bonn.de>
2026-04-04 23:30:02 +0200

    fix p213

--- PiBaseLean/Properties/P213/Defs.lean
+++ PiBaseLean/Properties/P213/Defs.lean
@@ -6,8 +6,8 @@ universe u
 
 namespace PiBase
 
-/- 210. α₁ space -/
-class α1Space (X : Type u) [τ : TopologicalSpace X] : Prop where
+/- 213. α₃ space -/
+class α3Space (X : Type u) [τ : TopologicalSpace X] : Prop where
   subset_converge {x : X} {S : ℕ → ℕ → X} (S_inj : ∀ n, Injective (S n))
     (hS : ∀ n : ℕ, Tendsto (S n) atTop (𝓝 x)) : ∃ T : ℕ → X,
       Tendsto T atTop (𝓝 x) ∧ range T ⊆ ⋃ n, range (S n) ∧
