59c2f7b95592c7941c038858e8a7f48c7ecc5561
Batixx <s59fpern@uni-bonn.de>
2026-04-05 19:04:09 +0200

    slight change for semilocally simply connected

--- PiBaseLean/Properties/P229/Defs.lean
+++ PiBaseLean/Properties/P229/Defs.lean
@@ -2,13 +2,13 @@ import Mathlib.AlgebraicTopology.FundamentalGroupoid.FundamentalGroup
 
 universe u
 
-open Topology Filter
+open Topology
 
 namespace PiBase
 
 /- 229. Semilocally simply conneced -/
 class SemilocallySimplyConnectedSpace (X : Type u) [TopologicalSpace X] : Prop where
-  contractible_nbhd (x : X) : ∃ U : Set X, ∃ hU : U ∈ 𝓝 x,
+  homo_trivial (x : X) : ∃ U : Set X, ∃ hU : U ∈ 𝓝 x,
     (FundamentalGroup.map (⟨Subtype.val, continuous_subtype_val⟩ : C(U, X))
       ⟨x, mem_of_mem_nhds hU⟩).range = ⊥
 
