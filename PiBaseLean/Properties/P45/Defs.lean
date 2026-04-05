71f3e25c020e063751e238b7a7d02412f446bfbd
yhx-12243 <yhx12243@gmail.com>
2026-04-05 22:52:32 +0800

    Update PiBaseLean/Properties/P45/Defs.lean
    Co-authored-by: Felix Pernegger <s59fpern@uni-bonn.de>

--- PiBaseLean/Properties/P45/Defs.lean
+++ PiBaseLean/Properties/P45/Defs.lean
@@ -5,7 +5,7 @@ open Topology Set Function Filter TopologicalSpace
 namespace PiBase
 
 /- 45. Has a dispersion point -/
-class HasDispersionPoint (X : Type*) [TopologicalSpace X] extends PreconnectedSpace X where
-  ex_dispersion_point : ∃ p : X, IsTotallyDisconnected ({p}ᶜ)
+class HasDispersionPoint (X : Type*) [TopologicalSpace X] extends ConnectedSpace X where
+  ex_dispersion_point : ∃ p : X, IsTotallyDisconnected {p}ᶜ
 
 end PiBase
