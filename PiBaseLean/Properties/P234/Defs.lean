f19f505e492a8acbc43092a8b0b36381392f2d3e
Batixx <s59fpern@uni-bonn.de>
2026-04-05 05:09:12 +0200

    fix name of p234

--- PiBaseLean/Properties/P234/Defs.lean
+++ PiBaseLean/Properties/P234/Defs.lean
@@ -5,7 +5,7 @@ universe u
 namespace PiBase
 
 /- 234. Has open connected components -/
-class CountablyMetacompactSpace (X : Type u) [TopologicalSpace X] : Prop where
+class HasOpenConnectedComponents (X : Type u) [TopologicalSpace X] : Prop where
   component_open (x : X) : IsOpen (connectedComponent x)
 
 end PiBase
