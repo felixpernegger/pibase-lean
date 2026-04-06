4992dcd75c08612b39b2497b2be8806735636002
Felix Pernegger <s59fpern@uni-bonn.de>
2026-04-06 06:02:33 +0200

    Fix P222 (#241)
    * fix p222
    
    * mk all
    
    * fix p125

--- PiBaseLean/Properties/P125/Defs.lean
+++ PiBaseLean/Properties/P125/Defs.lean
@@ -2,7 +2,7 @@ import Mathlib.Logic.Nontrivial.Defs
 
 namespace PiBase
 
-/- 128. Has multiple points -/
+/- 125. Has multiple points -/
 #check Nontrivial
 
 end PiBase
--- PiBaseLean/Properties/P222/Defs.lean
+++ PiBaseLean/Properties/P222/Defs.lean
@@ -1,12 +1,11 @@
-import Mathlib.Topology.NoetherianSpace
-
-open TopologicalSpace Set
+import Mathlib.Data.Finite.Defs
+import Mathlib.Topology.Defs.Basic
 
 universe u
 
 namespace PiBase
 
-/- 224. Has cofinite topology -/
+/- 222. Has cofinite topology -/
 class HasCofiniteTopology (X : Type u) [TopologicalSpace X] : Prop where
   open_iff_cofinite (s : Set X) : IsOpen s ↔ sᶜ.Finite
 
