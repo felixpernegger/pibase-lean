5cbb4fe707a5fdd7057ef089d1ae5ce9fae610ab
Batixx <s59fpern@uni-bonn.de>
2026-04-06 05:14:12 +0200

    fix p1

--- PiBaseLean/Properties/P1/Defs.lean
+++ PiBaseLean/Properties/P1/Defs.lean
@@ -1,8 +1,17 @@
-import Mathlib.Topology.Defs.Filter
+import Mathlib.Topology.Separation.Basic
 
 namespace PiBase
 
-/- 130. Locally compact space -/
-#check LocallyCompactSpace
+/- 1. T₀-Space -/
+#check T0Space
 
 end PiBase
+
+namespace PiBase.Formal
+
+abbrev P1 := T0Space
+
+class NP1 (X : Type*) [TopologicalSpace X] where
+  not_p1 : ¬ P1 X
+
+end PiBase.Formal
