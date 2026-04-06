43ce8b94c6b531ad765b0d9c3aeb9d179d2db53f
Batixx <s59fpern@uni-bonn.de>
2026-04-06 02:48:42 +0200

    adapt proof t818

--- PiBaseLean/Theorems/T818/Theorem.lean
+++ PiBaseLean/Theorems/T818/Theorem.lean
@@ -7,13 +7,12 @@ namespace PiBase
 
 /- Theorem 818: a finite space is Toronto. -/
 instance instTorontoSpaceOfFinite (X : Type*) [TopologicalSpace X] [Finite X] : TorontoSpace X where
-  toronto Y hY := by
+  toronto Y hY :=
     have eq : Y = .univ := by
       apply (Set.eq_univ_iff_ncard Y).mpr
       change (#↑Y).toNat = Nat.card X
       rw [hY]
       exact rfl
-    rw [eq]
-    exact Homeomorph.Set.univ X
+    eq ▸ (Nonempty.intro <| Homeomorph.Set.univ X)
 
 end PiBase
