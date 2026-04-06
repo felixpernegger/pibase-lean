e1f57f9c824d1260e35fd5c0eead02a3ef17cfe2
Batixx <s59fpern@uni-bonn.de>
2026-04-06 02:37:01 +0200

    rename t390

--- PiBaseLean/Theorems/T390/Theorem.lean
+++ PiBaseLean/Theorems/T390/Theorem.lean
@@ -8,8 +8,8 @@ open Cardinal
 namespace PiBase
 
 /-- Theorem 390: |X| ≤ 𝔠 implies |X| ≤ 2^𝔠 -/
-instance instCardLeContinuumOfCardEqContinuum (X : Type u) [h : CardLeContinuum X] :
-    CardLtPowerContinuum X where
+instance instCardLeContinuumOfCardLePowerContinuum (X : Type u) [h : CardLeContinuum X] :
+    CardLePowerContinuum X where
   card_lt := lt_of_le_of_lt h.card_le (cantor 𝔠)
 
 end PiBase
