9415d6b3c24c30fa3e8656e196d8f105d9168e84
yhx-12243 <yhx12243@gmail.com>
2026-04-06 12:44:17 +0800

    fix: T391

--- PiBaseLean/Theorems/T391/Theorem.lean
+++ PiBaseLean/Theorems/T391/Theorem.lean
@@ -1,3 +1,4 @@
+import PiBaseLean.Properties.Bundled.Basic
 import PiBaseLean.Properties.P58.Defs
 import PiBaseLean.Properties.P65.Defs
 import PiBaseLean.Properties.P163.Defs
@@ -11,12 +12,17 @@ namespace PiBase
 --TODO: Maybe redo this once negations are properly implemented
 /-- Theorem 391: |X| ≤ 𝔠 and ¬ |X| < 𝔠  implies |X| = 𝔠 -/
 instance instCardEqContinuumOfCardLeContinuumOfNotCardltContinuum (X : Type u)
-    [h : CardLeContinuum X] [h' : NP58 X] : CardEqContinuum X where
+    [h : CardLeContinuum X] (h' : ¬CardLtContinuum X) : CardEqContinuum X where
   card_eq := by
-    apply le_antisymm
-    · exact h.card_le
-    · have := h'.not_p58
-      contrapose! this
-      exact { card_lt := this }
+    refine le_antisymm h.card_le ?_
+    contrapose! h'
+    exact ⟨h'⟩
 
 end PiBase
+
+namespace PiBase.Formal
+
+theorem T391 : P163 ⊓ P58ᶜ ≤ P65 := fun X _ ↦ and_imp.2
+  (@instCardEqContinuumOfCardLeContinuumOfNotCardltContinuum X)
+
+end PiBase.Formal
