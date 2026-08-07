37ae0c63fc870b52c252a86a0736b01b9a14fb9f
Batixx <s59fpern@uni-bonn.de>
2026-08-08 00:56:19 +0200

    fix

--- PiBaseLean/Properties/P156/Defs.lean
+++ PiBaseLean/Properties/P156/Defs.lean
@@ -14,7 +14,7 @@ open Set
 /- 156. k-Rothberger -/
 class KRothbergerSpace (X : Type u) [TopologicalSpace X] : Prop where
   k_rothberger : ∀ {ι : Type u} (U : ℕ → ι → Set X), (∀ n, IsKCover'' (U n)) →
-    ∃ j : ℕ → ι, univ = ⋃ (n : ℕ), U n (j n)
+    ∃ j : ℕ → ι, IsKCover'' (fun n ↦ (U n) (j n))
 
 end PiBase
 
--- PiBaseLean/Properties/P159/Defs.lean
+++ PiBaseLean/Properties/P159/Defs.lean
@@ -14,7 +14,7 @@ open Set
 /- 159. k-Menger -/
 class KMengerSpace (X : Type u) [TopologicalSpace X] : Prop where
   k_menger : ∀ {ι : Type u} (U : ℕ → ι → Set X), (∀ (n : ℕ), IsKCover'' (U n)) →
-    ∃ s : ℕ → Finset ι, univ = ⋃ n : ℕ, ⋃ i ∈ s n, U n i
+    ∃ s : ℕ → Finset ι, IsKCover' {U n i | (n : ℕ) (i : ι) (_ : i ∈ s n)}
 
 end PiBase
 
